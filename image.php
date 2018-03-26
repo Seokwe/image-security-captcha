80211af53f33ec5bcf58e7d1b6403978eb5f9eb5
Lehlohonolo Seokwe <32381965+Seokwe@users.noreply.github.com>
2018-03-26 04:00:21 +0200

    Add files via upload

---
+++ image.php
@@ -0,0 +1,10 @@
+
+
+
+<form action="uploads/upload.php" method="POST" enctype="multipart/form-data">
+
+	<input type="file" name="file"><br><br>
+
+	<button type="submit" name="submit">Submit</button>
+	 
+</form>
